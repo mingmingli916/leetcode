658dc4701f7b0d2193adf06c0737ee54de623e0b
Mike Chyson <mike.chyson@gmail.com>
2021-04-09 09:13:25 +0800

    find the duplicate number (done)

--- 2021/04/20210409_find_the_duplicate_number.py
+++ 2021/04/20210409_find_the_duplicate_number.py
@@ -43,4 +43,25 @@ from typing import List
 
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        pass
+        left, right = 1, len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            count = 0
+            for num in nums:
+                if num <= mid:
+                    count += 1
+            if count <= mid:
+                left = mid + 1
+            else:
+                right = mid
+        return right
+
+
+if __name__ == '__main__':
+    solution = Solution()
+
+    nums = [1, 3, 4, 2, 2]
+    nums = [3, 1, 3, 4, 2]
+
+    ans = solution.findDuplicate(nums)
+    print(ans)
