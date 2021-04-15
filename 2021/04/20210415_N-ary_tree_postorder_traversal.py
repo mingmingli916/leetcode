f4bcbe14baf9389dc9ece25a0cfc2c4c890abb04
Mike Chyson <mike.chyson@gmail.com>
2021-04-15 14:56:09 +0800

    N-ary tree postorder traversal (recursive and trivial solution)

--- 2021/04/20210415_N-ary_tree_postorder_traversal.py
+++ 2021/04/20210415_N-ary_tree_postorder_traversal.py
@@ -1,5 +1,5 @@
 """
-Given the root of an n-ary tree, return the preorder traversal of its nodes' values.
+Given the root of an n-ary tree, return the postorder traversal of its nodes' values.
 
 Nary-Tree input serialization is represented in their level order traversal.
 Each group of children is separated by the null value (See examples)
@@ -9,28 +9,25 @@ Each group of children is separated by the null value (See examples)
 Example 1:
 
 
-
 Input: root = [1,null,3,2,4,null,5,6]
-Output: [1,3,5,6,2,4]
+Output: [5,6,3,2,4,1]
 Example 2:
 
 
-
 Input: root = [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
-Output: [1,2,3,6,7,11,14,4,8,12,5,9,13,10]
+Output: [2,6,14,11,7,3,12,8,4,13,9,10,5,1]
 
 
 Constraints:
 
-The number of nodes in the tree is in the range [0, 10^4].
-0 <= Node.val <= 10^4
+The number of nodes in the tree is in the range [0, 104].
+0 <= Node.val <= 104
 The height of the n-ary tree is less than or equal to 1000.
 
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
 """
 from typing import List
-import collections
 
 
 # Definition for a Node.
@@ -41,26 +38,26 @@ class Node:
 
 
 class Solution:
-    def preorder(self, root: 'Node') -> List[int]:
+    def postorder(self, root: 'Node') -> List[int]:
         if root is None:
             return []
 
-        lst = [root.val]
+        lst = []
         for child in root.children:
-            lst.extend(self.preorder(child))
-
+            lst.extend(self.postorder(child))
+        lst.append(root.val)
         return lst
 
-    def preorder_iterative(self, root: 'Node') -> List[int]:
+    def postorder(self, root: 'Node') -> List[int]:
         if root is None:
             return []
 
-        # Store visited nodes.
         stack = []
         stack.append(root)
         result = []
+
         while stack:
             node = stack.pop()
-            result.append(node.val)
-            stack.extend(node.children[::-1])
+            result.insert(0, node.val)
+            stack.extend(node.children)
         return result
