ec85e9d4f903bfbf44e1f991842cfe37c4f21d2a
Mike Chyson <mike.chyson@gmail.com>
2021-04-16 11:42:05 +0800

    validate binary search tree

---
+++ 2021/04/20210416_validate_binary_search_tree.py
@@ -0,0 +1,65 @@
+"""
+Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+
+A valid BST is defined as follows:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+
+Example 1:
+
+
+Input: root = [2,1,3]
+Output: true
+Example 2:
+
+
+Input: root = [5,1,4,null,null,3,6]
+Output: false
+Explanation: The root node's value is 5 but its right child's value is 4.
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 10^4].
+-2^31 <= Node.val <= 2^31 - 1
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    # WRONG!
+    # def isValidBST(self, root: TreeNode) -> bool:
+    #     if root is None:
+    #         return True
+    #
+    #     left_bool = True
+    #     right_bool = True
+    #     if root.left:
+    #         left_bool = root.left.val <= root.val and self.isValidBST(root.left)
+    #     if root.right:
+    #         right_bool = root.right.val >= root.val and self.isValidBST(root.right)
+    #
+    #     return left_bool and right_bool
+
+    def isValidBST(self, root: TreeNode) -> bool:
+        def helper(node, minimum, maximum):
+            # Base case.
+            if node is None:
+                return True
+
+            if node.val <= minimum or node.val >= maximum:
+                return False
+
+            return helper(node.left, minimum, node.val) and helper(node.right, node.val, maximum)
+
+        return helper(root, -float('inf'), float('inf'))
