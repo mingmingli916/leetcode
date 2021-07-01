784cd0220e62511e23535353fe6e39bbc5e98c08
Mike Chyson <mike.chyson@gmail.com>
2021-07-01 22:01:23 +0800

    add import

--- 2020/07/20200728-perfect-square.py
+++ 2020/07/20200728-perfect-square.py
@@ -1,4 +1,5 @@
-# Given a positive integer n, find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
+# Given a positive integer n,
+# find the least number of perfect square numbers (for example, 1, 4, 9, 16, ...) which sum to n.
 
 # Example 1:
 
@@ -12,24 +13,21 @@
 # Explanation: 13 = 4 + 9.
 
 
-
 # dynamic programming
+from math import ceil, sqrt
 
 
 class Solution:
     def numSquares(self, n: int) -> int:
-        dp = [0, 1, 2, 3]       # cache to save computation
+        dp = [0, 1, 2, 3]  # cache to save computation
         for i in range(4, n + 1):
-            dp.append(i)        # cache
+            dp.append(i)  # cache
             # go through all smaller numbers to recursive find the minimum
             for x in range(1, int(ceil(sqrt(i))) + 1):
                 tmp = x * x
                 if tmp > i:
                     break
                 else:
-                    dp[i] = min(dp[i], 1 + dp[i-tmp])
+                    dp[i] = min(dp[i], 1 + dp[i - tmp])
 
         return dp[n]
-            
-        
-        
