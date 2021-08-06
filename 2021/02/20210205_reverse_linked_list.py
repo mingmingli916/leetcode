d5bd547bb8b43ce48cd9c3141b85be58d2173808
Mingming Li <mingmingli916@gmail.com>
2021-08-06 17:35:21 -0400

    reverse linked list (solution 2)

--- 2021/02/20210205_reverse_linked_list.py
+++ 2021/02/20210205_reverse_linked_list.py
@@ -25,6 +25,9 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def __str__(self):
+        return str(self.val)
+
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
@@ -41,3 +44,29 @@ class Solution:
             return head
 
         return helper(None, head)
+
+
+class Solution2:
+    def reverseList(self, head: ListNode) -> ListNode:
+        prev = None
+        cur = head
+        while head:
+            head = head.next
+            cur.next = prev
+            prev = cur
+            cur = head
+        return prev
+
+
+if __name__ == '__main__':
+    s = Solution2()
+    head = ListNode(1)
+    cur = head
+    for i in range(2, 6):
+        cur.next = ListNode(i)
+        cur = cur.next
+    head = s.reverseList(head)
+
+    while head:
+        print(head)
+        head = head.next
