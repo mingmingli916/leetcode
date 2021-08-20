f0f714e86c6099caf496ad14a1d149eb39b2b687
Mingming Li <mingmingli916@gmail.com>
2021-08-20 13:44:13 -0400

    merge sort array, second solution

--- 2020/12/20201216-merge-sorted-array.py
+++ 2020/12/20201216-merge-sorted-array.py
@@ -54,6 +54,7 @@ class Solution:
                 else:
                     break
         print(nums1)
+        print(nums2)
 
         # Put elements in nums2 into nums1 according to
         # the index stored in nums1
@@ -78,6 +79,23 @@ class Solution:
             nums1[tmp] = nums2[i - m]
 
 
+class Solution2:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        i = m - 1
+        j = n - 1
+        last_index = n + m - 1
+
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[last_index] = nums1[i]
+                i -= 1
+            else:
+                nums1[last_index] = nums2[j]
+                j -= 1
+
+            last_index -= 1
+
+
 if __name__ == '__main__':
     nums1 = [1, 2, 3, 0, 0, 0]
     # nums1 = [1, 7, 10, 0, 0, 0]
@@ -95,10 +113,10 @@ if __name__ == '__main__':
     # nums2 = [2]
     # n = 1
     #
-    # nums1 = [1, 2, 4, 5, 6, 0, 0]
-    # m = 5
-    # nums2 = [3, 7]
-    # n = 2
-    s = Solution()
+    nums1 = [1, 2, 4, 5, 6, 0, 0]
+    m = 5
+    nums2 = [3, 7]
+    n = 2
+    s = Solution2()
     s.merge(nums1, m, nums2, n)
     print(nums1)
